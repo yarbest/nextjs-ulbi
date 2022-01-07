ec8cf73ab588fe53b58cb375c0ea76eb5ad47bb9
yarbest <yarbest15@gmail.com>
2022-01-07 13:52:47 +0200

    all done

---
+++ components/MainContainer.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import A from './A';
+
+const MainContainer = ({ children, keywords }) => {
+  return (
+    <>
+      <Head>
+        <meta keywords={'react, next ' + keywords}></meta>
+        <title>Главная страница</title>
+      </Head>
+
+      <div className="navbar">
+        <A href="/" text="Main" />
+        <A href="/users" text="Users" />
+      </div>
+
+      <div>{children}</div>
+
+      <style jsx>{`
+        .navbar {
+          background: orange;
+          padding: 15px;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default MainContainer;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import MainContainer from '../components/MainContainer';
+
+const Index = () => {
+  return (
+    <MainContainer keywords="main">
+      <h1>Main page</h1>
+    </MainContainer>
+  );
+};
+
+export default Index;
