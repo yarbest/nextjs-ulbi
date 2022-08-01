fe17321fa7ea1768e5b1add91b9816d668a3f7b7
yarbest <yarbest15@gmail.com>
2022-08-01 11:45:43 +0300

    Добавил линтер, теперь показывает больше предупреждений и ошибок

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import MainContainer from '../components/MainContainer';
 import s from '../styles/Index.module.scss';
+import image from '/public/images/img.jpg';
 
 const Index = () => {
   return (
     <MainContainer keywords="main">
       <h1>Main page</h1>
       <div className={s.imageContainer}>
-        <Image className={s.image} src="/images/img.jpg" width="522" height="522" />
+        <Image layout="responsive" className={s.image} src={image} />
       </div>
     </MainContainer>
   );
